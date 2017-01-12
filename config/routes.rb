b70346a94a27658a0d6a7bd83f6c801f31345ee9
takao saori <takao-saori@av.avex.co.jp>
2017-01-12 16:15:16 +0000

    ユーザ認証機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'books/index'
+
+  root to: 'books#index'
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
