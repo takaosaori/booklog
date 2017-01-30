64cbdd868369e66ff2ceec3385dfe8f0fc27bdd9
takao saori <takao-saori@av.avex.co.jp>
2017-01-30 14:39:26 +0000

    11_基本課題_修正

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,7 +1,7 @@
 module ReviewsHelper
     
   def review_average(reviews)
-    (reviews.length > 0 ? reviews.inject(0) {|sum, r| sum + r.rank}.to_f / reviews.length : 0) .round(2)
+    (reviews.length > 0 ? reviews.inject(0) {|sum, r| sum + r.rank}.to_f / reviews.length : 0).round(2)
   end
   
 end
